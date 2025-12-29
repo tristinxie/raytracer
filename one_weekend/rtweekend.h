d0304081b1950371840a99d521a2c575a7affc4c
tristinxie <tristinxie@gmail.com>
2025-12-28 23:31:46 -0800

    7 Refactor Complete

--- one_weekend/rtweekend.h
+++ one_weekend/rtweekend.h
@@ -26,5 +26,5 @@ inline double degrees_to_radians(double degrees) {
 #include "color.h"
 #include "ray.h"
 #include "vec3.h"
-
+#include "interval.h"
 #endif
--- one_weekend/sphere.h
+++ one_weekend/sphere.h
@@ -8,7 +8,7 @@ class sphere : public hittable {
 	public:
 	    sphere(const point3& center, double radius) : center(center), radius(std::fmax(0,radius)) {}
 
-		bool hit(const ray& r, double ray_tmin, double ray_tmax, hit_record& rec) const override {
+		bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
 			vec3 oc = center - r.origin();
 			auto a = r.direction().length_squared();
 			auto h = dot(r.direction(), oc);
@@ -22,9 +22,9 @@ class sphere : public hittable {
 			
 			// Find nearest that is within acceptable range.
 			auto root = (h - sqrtd) / a;
-			if (root <= ray_tmin || ray_tmax <= root) {
+			if (!ray_t.surrounds(root)) {
 				root = (h + sqrtd) / a;
-				if (root <= ray_tmin || ray_tmax <= root) {
+				if (!ray_t.surrounds(root)) {
 					return false;
 				}
 			} 
