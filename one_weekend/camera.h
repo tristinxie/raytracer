975de7a4cb1fbfe6925519b12a347743f4b371d3
tristinxie <tristinxie@gmail.com>
2025-12-29 16:40:04 -0800

    Finished 10 Metals

--- one_weekend/camera.h
+++ one_weekend/camera.h
@@ -2,12 +2,13 @@
 #define CAMERA_H
 
 #include "hittable.h"
-
+#include "material.h"
 class camera {
 	public:
-		double aspect_ratio = 1.0; // Ratio of image width over height
-		double image_width = 100; // Rendered image width in pixel count
-		int samples_per_pixel = 10;
+		double aspect_ratio = 1.0;  // Ratio of image width over height
+		double image_width = 100;   // Rendered image width in pixel count
+		int samples_per_pixel = 10; // Count of random samples per pixel
+		int max_depth         = 10; // Maximum number of ray bounces into scene
 		void render(const hittable& world) {
 			initialize();
 			std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
@@ -18,7 +19,7 @@ class camera {
 					color pixel_color(0,0,0);
 					for (int sample = 0; sample < samples_per_pixel; sample++) {
 						ray r = get_ray(col, row);
-						pixel_color += ray_color(r, world);
+						pixel_color += ray_color(r, max_depth, world);
 					}
 					write_color(std::cout, pixel_samples_scale * pixel_color);
 				}
@@ -72,10 +73,19 @@ class camera {
 			// Returns the vector to a random point in the [-.5, -.5]-[+.5, +.5] unit square
 			return vec3(random_double() - 0.5, random_double() - 0.5, 0);
 		}
-		color ray_color(const ray& r, const hittable& world) {
+		color ray_color(const ray& r, int depth, const hittable& world) {
+			// If we've exceeded ray bounce limit, no more light is gathered
+			if (depth <= 0) {
+				return color(0, 0, 0);
+			}
 			hit_record rec;
-			if (world.hit(r, interval(0, infinity), rec)) {
-				return 0.5 * (rec.normal + color(1,1,1));
+			if (world.hit(r, interval(0.001, infinity), rec)) {
+				ray scattered;
+				color attenuation;
+				if (rec.mat->scatter(r, rec, attenuation, scattered)) {
+					return attenuation * ray_color(scattered, depth-1, world);
+				}
+				return color(0, 0, 0);
 			}
 			vec3 unit_direction = unit_vector(r.direction());
 			auto a = 0.5 * (unit_direction.y() + 1.0);
