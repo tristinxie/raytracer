bab1fa98bba9df98c8ef20b80dee0f5aef7f5ae7
tristinxie <tristinxie@gmail.com>
2026-02-07 02:21:38 -0800

    Finished textures

--- the_next_week/texture.h
+++ the_next_week/texture.h
@@ -1,3 +1,4 @@
+#include "rtw_stb_image.h"
 #ifndef TEXTURE_H
 #define TEXTURE_H
 class texture {
@@ -40,5 +41,29 @@ class checker_texture : public texture {
 		double inv_scale;
 		shared_ptr<texture> even;
 		shared_ptr<texture> odd;
+};
+
+class image_texture : public texture {
+	public:
+		image_texture(const char* filename) : image(filename) {}
+
+		color value(double u, double v, const point3& p) const override {
+			// If we have no texture data, the return solid cyan as a debugging aid.
+			if (image.height() <= 0) return color(0,1,1);
+
+			// Clamp input texture coordinates to [0,1] x [1,0]
+			u = interval(0,1).clamp(u);
+			v = 1.0 - interval(0,1).clamp(v); // Flip V to iamge coordinates
+
+			auto i = int(u * image.width());
+			auto j = int(v * image.height());
+			auto pixel = image.pixel_data(i, j);
+
+			auto color_scale = 1.0 / 255.0;
+			return color(color_scale*pixel[0], color_scale*pixel[1], color_scale*pixel[2]);
+		}
+
+	private:
+		rtw_image image;
 };
 #endif
